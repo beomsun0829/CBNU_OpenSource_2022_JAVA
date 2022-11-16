de81617b1a3289b0f098149b1d489bc96a10a385
Hong Beomsun <ghsqjatns@naver.com>
2022-11-16 09:20:42 +0900

    cm

--- backend/backend/src/main/java/com/cbnuopensource2022java/BackendApplication.java
+++ backend/backend/src/main/java/com/cbnuopensource2022java/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.cbnuopensource2022java.backend;
+package com.cbnuopensource2022java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
